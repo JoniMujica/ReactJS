dc3c26c2fe9577f5f7287eb032e28184e598ddee
JoniMujica <jonimujica3@hotmail.com>
2022-06-11 02:25:21 -0300

    creacion y sintaxis JSX

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nombre = "Jon";
+  let auth = true;
+  let estaciones = ["Primavera","Verano","Otoño","Inverno"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor='nombre'>Nombre</label>
+        <input type="text" id='nombre'></input>
+        <h1>{nombre}</h1>
+        <p>{auth ? "El Usuario no esta Logueado":"El usuario esta logueado"}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {estaciones.map((el,index) =>(
+            <li key={index}>{el}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
