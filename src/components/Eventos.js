364052e35705250cd64f67e1dbd97b13fee5e804
JoniMujica <jonimujica3@otmail.com>
2023-03-15 17:31:59 -0300

    Property initializers

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-export default class Eventos extends Component{
+export class EventosES6 extends Component{
     constructor(props){
         super(props)
         this.state = {
@@ -27,12 +27,56 @@ export default class Eventos extends Component{
     render(){
         return(
             <div>
-                <h2>Eventos en componente de clase</h2>
+                <h2>Eventos en componente de clase ES6</h2>
                 <h3>{this.state.contador}</h3>
                 <nav>
                     <button onClick={this.sumar}>+</button>
                     <button onClick={this.restar}>-</button>
                 </nav>
+            </div>
+        )
+    }
+}
+
+export class EventosES7 extends Component{
+    state = {
+        contador:0,
+    }
+
+    sumar=(e) =>{
+        console.log("sumando")
+        console.log(this)
+        this.setState({
+            contador: this.state.contador + 1
+        })
+    }
+    restar=(e)=>{
+        console.log("restando")
+        console.log(this)
+        this.setState({
+            contador: this.state.contador - 1
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>Eventos en componente de clase ES7</h2>
+                <h3>{this.state.contador}</h3>
+                <nav>
+                    <button onClick={this.sumar}>+</button>
+                    <button onClick={this.restar}>-</button>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export class MasSobreEventos extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Mas Sobre Eventos</h3>
             </div>
         )
     }
