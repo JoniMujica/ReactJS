9ca80465a1acc3103705c9891d724f8545cc4cb6
JoniMujica <jonimujica3@hotmail.com>
2023-06-04 18:53:20 -0300

    Formularios + (Spread operator + destructuracion)

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import RelojHooks from './components/RelojHooks';
 import AjaxHooks from './components/AjaxHooks';
 import HooksPersonalizados from './components/HooksPersonalizados';
 import Referencias from './components/Referencias';
+import Formularios from './components/Formularios';
 
 
 function App() {
@@ -90,6 +91,8 @@ function App() {
           <HooksPersonalizados />
           <hr />
           <Referencias />
+          <hr />
+          <Formularios />
         </section>
       </header>
     </div>
