0567daf4dce87204a12e8c78ddf5ae297f0c451d
JoniMujica <jonimujica3@hotmail.com>
2023-06-04 21:24:07 -0300

    Estilos CSS + SASS

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import AjaxHooks from './components/AjaxHooks';
 import HooksPersonalizados from './components/HooksPersonalizados';
 import Referencias from './components/Referencias';
 import Formularios from './components/Formularios';
+import Estilos from './components/Estilos';
 
 
 function App() {
@@ -93,6 +94,8 @@ function App() {
           <Referencias />
           <hr />
           <Formularios />
+          <hr />
+          <Estilos />
         </section>
       </header>
     </div>
