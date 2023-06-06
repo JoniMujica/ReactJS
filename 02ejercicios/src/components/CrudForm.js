a234eea500e45ae7688524751da0070dfa7dc8ad
JoniMujica <jonimujica3@hotmail.com>
2023-06-06 09:12:10 -0300

    EJERCICIO CRUD 1/4 Creacion de componentes y renderizado de datos

---
+++ 02ejercicios/src/components/CrudForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+
+const initialForm={
+    name:"",
+    constellation:"",
+    id:null
+}
+
+
+export const CrudForm = () => {
+
+    const [form, setForm] = useState(initialForm);
+    const handleChange = (e) =>{}
+    const handleSubmit = (e) =>{}
+    const handleReset = (e) =>{}
+
+  return (
+    <div>
+        <h3>Agregar</h3>
+        <form onSubmit={handleSubmit}>
+            <input type='text' name='name' placeholder='Nombre' onChange={handleChange} value={form.name} />
+            <input type='text' name='constellation' placeholder='Constelacion' onChange={handleChange} value={form.constellation} />
+            <input type="submit" value="Enviar" />
+            <input type="reset" value="Limpiar" onClick={handleReset} />
+        </form>
+    </div>
+  )
+}
