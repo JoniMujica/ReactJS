9c5fd065d4a8eaed2fb9f99d8f2a3a652fcc221d
JoniMujica <jonimujica3@hotmail.com>
2023-07-08 21:58:47 -0300

    Implementando Loaders y Mensajes de errores ( 4 / 5 )

--- 02ejercicios/src/components/CrudApi.js
+++ 02ejercicios/src/components/CrudApi.js
@@ -1,30 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
+
 import { CrudForm } from './CrudForm'
 import { CrudTable } from './CrudTable'
 import { helpHttp } from '../helpers/helpHttp';
+import Loader from './Loader';
+import Message from './Message';
 
 
 
 export const CrudApi = () => {
 
     const [dataToEdit, setDataToEdit] = useState(null);
+    const [db, setDb] = useState(null)
 
-    const [db, setDb] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
  
-    let api = helpHttp();
+    //let api = helpHttp();
     let url = "http://localhost:5000/santos";
 
     useEffect(() => {
-        api.get(url).then((res)=>{
-            //console.log(res)
+        setLoading(true)
+        helpHttp().get(url).then((res)=>{
+            console.log(res)
             if(!res.err){
                 setDb(res);
+                setError(null)
             }else{
                 setDb(null);
+                setError(res);
             }
         });
-    }, []);
+
+
+        setLoading(false)
+    }, [url]);
     
     const createData = (data) => {
         data.id = Date.now();
@@ -50,7 +61,10 @@ export const CrudApi = () => {
         <h2>CRUD API</h2>
         <article className='grid-1-2'>
             <CrudForm createData={createData} updateData={updateData} dataToEdit={dataToEdit} setDataToEdit={setDataToEdit}/>
-            <CrudTable data={db} deleteData={deleteData} setDataToEdit={setDataToEdit}  />
+            {loading && <Loader />}
+            {error && <Message msg={`Error ${error.status} : ${error.statusText}`} bgColor="#dc3545" />}
+            {db && <CrudTable data={db} deleteData={deleteData} setDataToEdit={setDataToEdit}  />}
+            
         </article>
     </div>
   )
