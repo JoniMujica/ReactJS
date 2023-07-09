af84529c3e300d33cc24a8295856a36635285fff
JoniMujica <jonimujica3@hotmail.com>
2023-07-09 00:47:10 -0300

    Peticiones HTTP a la API ( 5 / 5 )

--- 02ejercicios/src/components/CrudApi.js
+++ 02ejercicios/src/components/CrudApi.js
@@ -15,43 +15,88 @@ export const CrudApi = () => {
     const [db, setDb] = useState(null)
 
     const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
  
-    //let api = helpHttp();
+    let api = helpHttp();
     let url = "http://localhost:5000/santos";
 
     useEffect(() => {
-        setLoading(true)
-        helpHttp().get(url).then((res)=>{
-            console.log(res)
-            if(!res.err){
-                setDb(res);
-                setError(null)
-            }else{
-                setDb(null);
-                setError(res);
+        setLoading(true);
+        helpHttp()
+          .get(url)
+          .then((res) => {
+            //console.log(res);
+            if (!res.err) {
+              setDb(res);
+              setError(null);
+            } else {
+              setDb(null);
+              setError(res);
             }
-        });
-
-
-        setLoading(false)
-    }, [url]);
+            setLoading(false);
+          });
+      }, [url]);
     
     const createData = (data) => {
+
+        let options ={
+            body: data,
+            headers: {"content-type":"application/json"}
+        }
         data.id = Date.now();
         //console.log(data)
-        setDb([...db,data])
+        api.post(url,options).then(res=>{
+            console.log(res)
+            if(!res.err){
+                setDb([...db,res])
+            }else{
+                setError(res)
+            }
+        })
+        
     }
     const updateData = (data) => {
-        let newData = db.map(el => el.id === data.id ? data : el);
-        setDb(newData);
+
+        let endpoint = `${url}/${data.id}`
+        //console.log(endpoint);
+
+        //let newData = db.map(el => el.id === data.id ? data : el);
+        //setDb(newData);
+
+        let options ={
+            body: data,
+            headers: {"content-type":"application/json"}
+        }
+        data.id = Date.now();
+        //console.log(data)
+        api.put(endpoint,options).then(res=>{
+            //console.log(res)
+            if(!res.err){
+                let newData = db.map(el => el.id === data.id ? data : el);
+                //setDb([...db,res])
+                setDb(newData)
+            }else{
+                setError(res)
+            }
+        })
     }
     const deleteData = (id) => {
         let isDelete = window.confirm(`¿Estas seguro que desea eliminar el registro con la ID '${id}'`);
         if (isDelete) {
-            let newData = db.filter(el=>el.id !== id)
-            setDb(newData)
 
+            let endpoint = `${url}/${id}`
+            let options ={
+                headers: {"content-type":"application/json"}
+            }
+            api.del(endpoint,options).then(res=>{
+                if(!res.err){
+                    let newData = db.filter(el=>el.id !== id)
+                    setDb(newData)
+                }else{
+                    setError(res)
+                }
+            })
+            
         } else {
             return; 
         }
--- 02ejercicios/src/components/CrudTable.js
+++ 02ejercicios/src/components/CrudTable.js
@@ -14,7 +14,7 @@ export const CrudTable = ({data,setDataToEdit,deleteData}) => {
                 </tr>
             </thead>
             <tbody>
-                {data.length === 0 ? <tr><td colSpan="3">Sin Datos</td></tr> : data.map(el=> <CrudTableRow key={el.id} el={el} setDataToEdit={setDataToEdit} deleteData={deleteData} />)}
+                {data.length > 0 ?data.map(el=> (<CrudTableRow key={el.id} el={el} setDataToEdit={setDataToEdit} deleteData={deleteData}/>)) : <tr><td colSpan="3">Sin Datos</td></tr>}
             </tbody>
         </table>
     </div>
