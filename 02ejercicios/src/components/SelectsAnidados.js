10e997142da194513325638ad7f538222a68bd6f
JoniMujica <jonimujica3@hotmail.com>
2024-05-22 16:53:20 -0300

    Selects Anidados: Definición de componentes y lógica

--- 02ejercicios/src/components/SelectsAnidados.js
+++ 02ejercicios/src/components/SelectsAnidados.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useState} from "react"
+import SelectList from './SelectList'
 
 const SelectsAnidados = () => {
+    const [state, setState] = useState("");
+    const [town, setTown] = useState("");
+    const [suburb, setSuburb] = useState("");
   return (
     <div>
         <h2>Selects Anidados</h2>
+        <h3>Mexico</h3>
+        <SelectList title="" url="" handleChange={(e) => {setState(e.target.vaalue)}} />
+        {state && <SelectList title="" url="" handleChange={(e) => {setTown(e.target.vaalue)}} />}
+        {town && <SelectList title="" url="" handleChange={(e) => {setSuburb(e.target.vaalue)}} />}
+        <pre>
+            <code>
+                {state} - {town} - {suburb}
+            </code>
+        </pre>
     </div>
   )
 }
