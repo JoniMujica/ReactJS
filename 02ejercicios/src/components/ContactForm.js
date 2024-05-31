bc01aa946f89a46e1c497e92c223d074956b661d
JoniMujica <jonimujica3@hotmail.com>
2024-05-31 11:46:11 -0300

    Validación Formularios: Definición de componentes y lógica ( 1 / 4 )

---
+++ 02ejercicios/src/components/ContactForm.js
@@ -0,0 +1,22 @@
+import { useForm } from "../hooks/useForm"
+
+const initialForm = {}
+const validationsForm = (form) =>{}
+const ContactForm = () => {
+
+    const {form,errors,loading,response,handleChange,handleBlur,handleSubmmit} = useForm(initialForm,validationsForm);
+  return (
+    <div>
+        <h2>Formulario de contacto</h2>
+        <form onSubmit={handleSubmmit}>
+                <input type="text" name="name" placeholder="Escribe tu nobre" onBlur={handleBlur} onChange={handleChange} value={form.name} required />
+                <input type="email" name="email" placeholder="Escribe tu Email" onBlur={handleBlur} onChange={handleChange} value={form.email} required />
+                <input type="text" name="subject" placeholder="Asunto a tratar" onBlur={handleBlur} onChange={handleChange} value={form.subject} required />
+                <textarea name="comments" cols="50" rows="5" placeholder="Escribe tus comentarios"onBlur={handleBlur} onChange={handleChange} value={form.comments} required></textarea>
+                <input type="submit" value="Enviar" />
+        </form>
+    </div>
+  )
+}
+
+export default ContactForm
