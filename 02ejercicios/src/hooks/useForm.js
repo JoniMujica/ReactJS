0cd9a5fee96d58fa4da0c862d71d545c8ef00d13
JoniMujica <jonimujica3@hotmail.com>
2024-06-01 13:10:45 -0300

    Validación Formularios: Envío de datos AJAX y API FormSubmit ( 4 / 4 )

--- 02ejercicios/src/hooks/useForm.js
+++ 02ejercicios/src/hooks/useForm.js
@@ -1,26 +1,59 @@
-import {useState} from 'react'
+import { useState } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 
-export const useForm = (initialForm,validateForm) => {
-    const [form, setForm] = useState(initialForm)
-    const [errors, setErrors] = useState({});
-    const [loading, setLoading] = useState(false);
-    const [response, setResponse] = useState(null);
+export const useForm = (initialForm, validateForm) => {
+  const [form, setForm] = useState(initialForm);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState(null);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-    const handleChange = (e) => {
-        const {name,value} = e.target;
-        setForm({
-            ...form,[name]:value
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleBlur = (e) => {
+    handleChange(e);
+    setErrors(validateForm(form));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateForm(form));
+
+    if (Object.keys(errors).length === 0) {
+      alert("Enviando Formulario");
+      setLoading(true);
+      helpHttp()
+        .post("https://formsubmit.co/ajax/jonimujica34@gmail.com", {
+          body: form,
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
         })
+        .then((res) => {
+          setLoading(false);
+          setResponse(true);
+          setForm(initialForm);
+          setTimeout(() => setResponse(false), 5000);
+        });
+    } else {
+      return;
     }
-    const handleBlur = (e) => {
-        handleChange(e);
-        setErrors(validateForm(form));
-    }
-    const handleSubmmit = (e) => {}
+  };
 
-    return {
-        form,errors,loading,response,handleChange,handleBlur,handleSubmmit
-    };
-
-}
+  return {
+    form,
+    errors,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  };
+};
